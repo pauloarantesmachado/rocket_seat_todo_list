5c04f40b724af4c810775111528f1181650172c3
pauloarantesmachado <pauloantoniomachado@hotmail.com>
2023-10-14 13:09:30 -0300

    update

--- src/main/java/br/com/pauloarantes/todolist/task/TaskController.java
+++ src/main/java/br/com/pauloarantes/todolist/task/TaskController.java
@@ -41,19 +41,22 @@ public class TaskController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity updateTask(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){
-        var idUser = request.getAttribute("idUser");
+    public ResponseEntity updateTask(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request){
         var task = this.taskRepository.findById(id).orElse(null);
+
         if(task == null){
             return  ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body("Tarefa não encontrada");
         }
+
+        var idUser = request.getAttribute("idUser");
+
         if(!task.getIdUSer().equals(idUser)){
             return  ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body("Usuário não tem permissão para alterar essa tarefa ");
         }
         Utils.copyNonNullProperties(taskModel, task);
         var responseTask = this.taskRepository.save(task);
-        return ResponseEntity.ok().body(responseTask);
+        return ResponseEntity.ok().body(this.taskRepository.save(responseTask));
     }
 }
